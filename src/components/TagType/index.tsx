b06e549321a15ac2518d5beda95e0aa19d47ba54
vilsonpneto <vilson.neto57@gmail.com>
2024-03-27 08:52:04 -0300

    feat: Created TagType component

---
+++ src/components/TagType/index.tsx
@@ -0,0 +1,13 @@
+import { capitalize } from "@/utils/functions/capitalize"
+import styles from './styles.module.css';
+interface ITagTypeProps {
+    type: string
+}
+
+export const TagType = ({type}: ITagTypeProps) => {
+    return (
+        <p className={`w-2/5 mr-2 text-sm text-center border-l border-gray-50 rounded-sm ${styles[type]}`}>
+            {capitalize(type)}
+        </p>
+    )
+} 
