b6a790f372bdf7ab86e22c6fe80ea459162502d9
vilsonpneto <vilson.neto57@gmail.com>
2024-03-27 08:58:18 -0300

    feat: Add animation in logo Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,23 @@ import Image from 'next/image';
 import Logo from "../../../public/pokebola.png"
 import 'tailwindcss/tailwind.css'
 
+import { motion } from "framer-motion";
+import { AnimatedTextWord } from '../AnimatedTextWord';
+
 interface IHeaderProps {
     children: React.ReactNode
 }
 
 export const Header = ({children}: IHeaderProps) => {
+
+    const text = "Pokedex".split("");
   
     return (
-      <header className="bg-red text-white p-4 flex items-center">
+      <header className="bg-dark-red text-white p-4 flex items-center w-full">
         <Image alt='pokeball fast' src={Logo} width={70} height={70}/>
-        <h1 className="text-2xl font-bold pl-4">Pokedex</h1>
+        <h1 className="text-2xl font-bold pl-4">
+            <AnimatedTextWord text='Pokedex' splitText=''/>
+        </h1>
         {children}
       </header>
     );
