0987208b115506365168bcfdb03baba711724420
vilsonpneto <vilson.neto57@gmail.com>
2024-03-27 08:49:03 -0300

    fix: Changed sprite type in Pokemon typing

--- src/interfaces/pokemon.d.ts
+++ src/interfaces/pokemon.d.ts
@@ -5,8 +5,7 @@ export interface IPokemon {
     height: number;
     weight: number;
     sprites: {
-      front_default: string;
-      back_default: string;
+      other: any
     };
     abilities: {
       name: string;
