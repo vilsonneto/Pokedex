24b89c3447a6525c85b6f4f102d073d6e95ae2ab
vilsonpneto <vilson.neto57@gmail.com>
2024-03-27 08:54:49 -0300

    feat: Created AnimatedTextWord component

---
+++ src/components/AnimatedTextWord/index.tsx
@@ -0,0 +1,58 @@
+import { motion } from "framer-motion";
+
+interface IAnimatedTextWord {
+    text: string
+    splitText: "" | " "
+}
+
+export const AnimatedTextWord = ({ text, splitText }: IAnimatedTextWord) => {
+  const words = text.split(splitText);
+
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 10,
+        stiffness: 50,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 10,
+      transition: {
+        type: "spring",
+        damping: 10,
+        stiffness: 100,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {words.map((word, index) => (
+        <motion.span
+          variants={child}
+          style={{ marginRight: "5px" }}
+          key={index}
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+};
