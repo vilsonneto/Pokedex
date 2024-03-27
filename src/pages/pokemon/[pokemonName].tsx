4eeca9319a149403f7e1d7a1e79cb19191ec06cf
vilsonpneto <vilson.neto57@gmail.com>
2024-03-27 15:58:38 -0300

    feat: Create dynamic Pokemon page

---
+++ src/pages/pokemon/[pokemonName].tsx
@@ -0,0 +1,37 @@
+import { IPokemon } from "@/interfaces/pokemon";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import "../../app/globals.css";
+import { Header } from "@/components/Header";
+import { PokemonDetails } from '../../components/PokemonDetails/index';
+
+export default function DynamicPage() {
+  const router = useRouter();
+  const { pokemonName } = router.query;
+
+  const [currentPokemon, setCurrentPokemon] = useState<IPokemon>();
+
+  useEffect(() => {
+    const fetchPokemon = async (name: string | string[] | undefined) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const data = await response.json();
+      setCurrentPokemon(data);
+    };
+
+    fetchPokemon(pokemonName);
+  }, [pokemonName]);
+
+  return (
+    <>
+      <Header>{""}</Header>
+      <main className="flex flex-col items-center justify-between">
+        {
+            currentPokemon && (
+                <PokemonDetails pokemon={currentPokemon}/>
+            )
+        }
+      </main>
+    </>
+  );
+}
