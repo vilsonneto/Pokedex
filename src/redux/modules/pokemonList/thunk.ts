3a50bd024d2b5e44fa5627c4d595fd90ef54d4c3
vilsonpneto <vilson.neto57@gmail.com>
2024-03-29 00:34:52 -0300

    refactor: Update logic fetchSearchPokemon

--- src/redux/modules/pokemonList/thunk.ts
+++ src/redux/modules/pokemonList/thunk.ts
@@ -18,50 +18,53 @@ async function fetchPokemonListDetails(offset: number) {
   return pokemons;
 }
 
-export const fetchPokemonListDetailsAsync = createAsyncThunk<
-  IPokemon[],
-  number
->(
+export const fetchPokemonListDetailsAsync = createAsyncThunk(
   "pokemonList/fetchPokemonListDetails",
   async (offset: number) => await fetchPokemonListDetails(offset)
 );
 
 async function fetchSearchPokemon(input: string) {
-  console.log("input:", input);
-  input = input.toLowerCase();
-  let dataPokemons = [];
-
-  if (typesPokemon.includes(input)) {
-    const data = await fetch(`https://pokeapi.co/api/v2/type/${input}`);
-    dataPokemons = await data.json();
-    dataPokemons = dataPokemons.pokemon.map(
-      ({ pokemon }: { pokemon: IPokemon }) => pokemon
-    );
-  } else {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1400`);
-    dataPokemons = await data.json();
-    // await storage.setItem("@POKEMON_SEARCH", dataJson)
-    dataPokemons = dataPokemons.results.filter((pokemon: IPokemon) => {
-      const { name } = pokemon;
-      const regex = new RegExp(input);
-      return regex.test(name);
+  try {
+    console.log("input:", input);
+    input = input.toLowerCase();
+    let dataPokemons = [];
+  
+    if (typesPokemon.includes(input)) {
+      const data = await fetch(`https://pokeapi.co/api/v2/type/${input}`);
+      dataPokemons = await data.json();
+      dataPokemons = dataPokemons.pokemon.map(
+        ({ pokemon }: { pokemon: IPokemon }) => pokemon
+      );
+    } else {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1400`);
+      dataPokemons = await data.json();
+      // await storage.setItem("@POKEMON_SEARCH", dataJson)
+      dataPokemons = dataPokemons.results.filter((pokemon: IPokemon) => {
+        const { name } = pokemon;
+        const regex = new RegExp(input);
+        return regex.test(name);
+      });
+    }
+  
+    // console.log("dataPokemons:", dataPokemons);
+  
+    const pokemonPromises = await dataPokemons.map(async (pokemon: IPokemon) => {
+      const pokemonDetails = await fetch(pokemon.url);
+      const pokemonJson = await pokemonDetails.json();
+      return pokemonJson;
     });
+  
+    const pokemons = await Promise.all(pokemonPromises);
+    console.log("pokemons:", pokemons);
+    return pokemons;
+  } catch(err) {
+    console.error("Pokemon Not Found")
   }
 
-  // console.log("dataPokemons:", dataPokemons);
-
-  const pokemonPromises = await dataPokemons.map(async (pokemon: IPokemon) => {
-    const pokemonDetails = await fetch(pokemon.url);
-    const pokemonJson = await pokemonDetails.json();
-    return pokemonJson;
-  });
-
-  const pokemons = await Promise.all(pokemonPromises);
-  console.log("pokemons:", pokemons);
-  return pokemons;
+  
 }
 
-export const fetchSearchPokemonAsync = createAsyncThunk<IPokemon[], string>(
+export const fetchSearchPokemonAsync = createAsyncThunk(
   "pokemonList/fetchSearchPokemon",
   async (input: string) => await fetchSearchPokemon(input)
 );
