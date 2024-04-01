86e60ad62d64ebe26ebe93fd63c58b559cdd1ed6
vilsonpneto <vilson.neto57@gmail.com>
2024-04-01 08:20:42 -0300

    fix: Remove webpack of nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
     images: {
       remotePatterns: [
         {
