da0f9b9d1aaa733a122957715ffa9131a952fcdd
vilsonpneto <vilson.neto57@gmail.com>
2024-04-04 16:46:30 -0300

    refactor: Remove comments of stories

--- src/stories/PokemonNotFound.stories.tsx
+++ src/stories/PokemonNotFound.stories.tsx
@@ -1,20 +1,14 @@
-import { mockPokemonList } from "@/src/__mocks__/mockPokemonList";
 import { PokemonNotFound } from "../components/PokemonNotFound";
 
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/PokemonNotFound',
   component: PokemonNotFound,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     pokemon: [],
   },
@@ -23,11 +17,10 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const notFoundPokemon: Story = {
+export const NotFoundPokemon: Story = {
   args: {
     pokemon: [],
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 
