4293690ee1a5947fc6518f52012ff30172ccb60a
vilsonpneto <vilson.neto57@gmail.com>
2024-05-10 01:06:16 -0300

    feat: Create unitary test PokemonList component

--- src/components/PokemonList/index.test.tsx
+++ src/components/PokemonList/index.test.tsx
@@ -5,7 +5,7 @@ import { mockPokemonList } from "@/src/__mocks__/mockPokemonList";
 import { useRouter, NextRouter } from "next/router";
 import { PokemonList } from ".";
 import "@testing-library/jest-dom";
-import 'intersection-observer';
+import "intersection-observer";
 
 // jest.mock("@lottiefiles/react-lottie-player", () => ({
 //   __esModule: true,
@@ -22,31 +22,23 @@ const initialState = {
 
 const store = mockStore(initialState);
 
-describe("PokemonList component", () => {
-  //   beforeEach(() => {
-  //     const mockUseRouter = jest.spyOn(require("next/router"), "useRouter");
-
-  //     mockUseRouter.mockReturnValueOnce({
-  //       pathname: "/",
-  //       query: {},
-  //     } as unknown as NextRouter);
-  //   });
-
-  it("renders the PokemonList component with correct data", async () => {
-    await render(
+describe("Render PokemonList component ", () => {
+  const renderComponent = () =>
+    render(
       <Provider store={store}>
         <PokemonList />
       </Provider>
     );
 
-    await waitFor(() => {
-        expect(screen.getByText("Bulbasaur")).toBeInTheDocument();
-        });
-        
+  it("with correct data", () => {
+    renderComponent();
+
+    expect(screen.getByText("Bulbasaur")).toBeInTheDocument();
+  });
+
+  it("with correct lenght", () => {
+    renderComponent();
+
     expect(screen.getAllByTestId("pokemon-card").length).toBe(3);
-
-    // expect(screen.getByText("Mock Player")).toBeInTheDocument();
-
-    // Add more assertions to check for other elements and properties
   });
 });
